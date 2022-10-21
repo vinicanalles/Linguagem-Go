5270152f45eb8d880684c24336e7197f6cfcdb5a
vinicanalles <62850892+vinicanalles@users.noreply.github.com>
2022-10-21 16:46:53 -0300

    Concorrência
    - Aprendendo conceitos sobre concorrência;
    - Aplicando Padrões de Concorrência;
    - Aplicando conceitos de Canais;

---
+++ 18 - Concorrência/6 - Padrões de Concorrência/6.1 - Worker Pools/worker-pools.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	tarefas := make(chan int, 45)
+	resultados := make(chan int, 45)
+
+	go worker(tarefas, resultados)
+	go worker(tarefas, resultados)
+	go worker(tarefas, resultados)
+	go worker(tarefas, resultados)
+
+	for i := 0; i < 45; i++ {
+		tarefas <- i
+	}
+
+	close(tarefas)
+
+	for i := 0; i < 45; i++ {
+		resultado := <-resultados
+		fmt.Println(resultado)
+	}
+}
+
+// tarefas <-chan int: canal que recebe dados
+// resultados chan<- int: canal que envia dados
+func worker(tarefas <-chan int, resultados chan<- int) {
+	for numero := range tarefas {
+		resultados <- fibonacci(numero)
+	}
+}
+
+func fibonacci(posicao int) int {
+	if posicao <= 1 {
+		return posicao
+	}
+
+	return fibonacci(posicao-2) + fibonacci(posicao-1)
+}
